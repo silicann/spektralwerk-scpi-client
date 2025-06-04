f47242c2edf4ef4b8fa844229c4859d04332dbbc
Daniel Cebulla <cebulla@silicann.com>
2025-06-04 09:56:29 +0200

    fix: correct wording for identity command
    The missing asterisk lead to a timeout when requesting the identity.

--- spektralwerk_scpi_client/scpi/commands.py
+++ spektralwerk_scpi_client/scpi/commands.py
@@ -19,11 +19,12 @@ class SCPICommand(enum.StrEnum):
         """
         return f"{self} {content}"
 
+    # SCPI standard/IEEE 488 commands
+    IDENTITY = "*IDN?"
+
     # SCPI commands
     DEVICE_SPECTROMETER_PIXELS_COUNT = "DEVice:SPECtrometer:PIXels:COUNt"
     DEVICE_SPECTROMETER_PIXELS_WAVELENGTH = "DEVice:SPECtrometer:PIXels:WAVelength"
-
-    IDENTITY = "IDN?"
 
     MEASURE_SPECTRUM_AVERAGE_NUMBER = "MEASure:SPECtrum:AVERage:NUMBer"
     MEASURE_SPECTRUM_EXPOSURE_TIME = "MEASure:SPECtrum:EXPosure:TIME"
